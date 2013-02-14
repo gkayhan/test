a4c0c9b88d2a6022aa280abf719546eca56cdb4e
gkayhan <gokhan.kayhan@bil.omu.edu.tr>
2013-02-14 04:44:47 -0800

    Create dataset_train_test.m

---
+++ dataset_train_test.m
@@ -0,0 +1,27 @@
+%% dataset: ds_train - ds_test
+clear all; close all; clc;
+ 
+ds = load('ds_reflectance.csv');
+N = size(ds, 1);
+r = randperm(N);
+ 
+ind = r(1:6000);
+ds_train.X = ds(ind, 1:4)';
+ds_train.y = ds(ind, 5:10)';
+ 
+ind = r(6001:end);
+ds_test.X = ds(ind, 1:4)';
+ds_test.y = ds(ind, 5:10)';
+%% dataset: ds_train - ds_test
+ds = load('ds_reflectance.csv');
+N = size(ds, 1);
+r = randperm(N);
+ 
+ind = r(1:6000);
+ds_train.X = ds(ind, 1:4)';
+ds_train.y = ds(ind, 5:10)';
+ 
+ind = r(6001:end);
+ds_test.X = ds(ind, 1:4)';
+ds_test.y = ds(ind, 5:10)';
+save('ds_train_test.mat', 'ds_train', 'ds_test');
